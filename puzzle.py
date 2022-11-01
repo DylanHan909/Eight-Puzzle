b60479ac4c8c9cb506967ecd2f2342eac0e81845
Dylan Han <dylanhan909@gmail.com>
2022-11-01 03:16:02 -0700

    Reordered some functions and modified comments to reflect new changes

--- puzzle.py
+++ puzzle.py
@@ -7,8 +7,8 @@ goal_state = ([1, 2, 3], [4, 5, 6], [7, 8, 0])
 verbose = False #A way to have debugging outputs for testing purposes
 
 #EMPTY TILE IS REPRESENTED BY 0
-#Node structure should include the following operators. Move up/down/left/right. 
-#Node structure should say: If the state was expanded or not. The heuristic cost for each node. (h(n)). The node depth
+#Changed node structure a lot
+#Keeps track of the puzzle state, the path taken to solve the puzzle, the heuristic from our algorithms (h(n)), and our current depth (g(n))
 class Node:
     def __init__ (self, puzzle):
         self.puzzle = puzzle
@@ -22,9 +22,10 @@ class Node:
         self.distance = self.depth + self.heuristic 
         other.distance = other.depth + other.heuristic
         if (other.distance == self.distance):
-            return other.depth > self.depth #Done because my search was bugged, when f(n) equaled, it would choose a larger depth randomly, choose the smaller depth manually to solve 
+            return other.depth > self.depth #Done because my search was bugged, when f(n) of two nodes equaled each other, it would choose a larger depth randomly, choose the smaller depth manually to solve 
         else:
             return other.distance > self.distance
+
 #Driver FUNCTION
 def main():
     '''
@@ -54,7 +55,7 @@ def search_puzzle(puzzle, algorithm):
     working_queue = PriorityQueue()
     repeated_states = [] #tried to use a set to speed up the process but it did not
     max_queue_size = 0 #Max queue size
-    expanded_nodes = 0 #Node count
+    expanded_nodes = 0 #Node count, changed from the old implementation as the old one was SUPER redundant, old nodes would NEVER be revisited with my dupe implementation, so there is no need for a separate attribute for expansion
 
     working_queue.put(curr_puzzle) #Put initial state into the priority queue
     repeated_states.append(curr_puzzle.puzzle) #put the initial states puzzle into the duplicate list
@@ -103,57 +104,6 @@ def node_expansion(puzzle, repeated_states, working_queue, algorithm):
         move_tile(puzzle, expand_row, expand_column, expand_row, (expand_column + 1), repeated_states, ' leftwards.', working_queue, algorithm)
     if (expand_column != 0): #If the column is to the right of tthe leftmost column
         move_tile(puzzle, expand_row, expand_column, expand_row, (expand_column - 1), repeated_states, ' rightwards.', working_queue, algorithm)
-
-#HEURISTIC FUNCTIONS
-def uniform_cost():
-    if (verbose):
-        print('Uniform Cost Search was selected.')
-    return 0 #Heuristic is always 0
-
-def a_star_misplaced(puzzle):
-    if (verbose):
-        print('Misplaced Tile Heuristic was selected.')
-    misplaced_tiles = 0
-    for row in range(len(puzzle)): #Loop through the puzzle
-        for column in range(len(puzzle)):
-            if (puzzle[row][column] != goal_state[row][column]):
-                if (puzzle[row][column] != 0): #need to account for an empty tile
-                    if (verbose):
-                        print('The misplaced tile is ' + str(puzzle[row][column]))
-                    misplaced_tiles += 1
-    if (verbose):
-        print("A total of " + str(misplaced_tiles) + " misplaced tiles were found." + '\n')
-    return misplaced_tiles
-
-def a_star_manhatten(puzzle):
-    #[1, 2, 3]    [3, 2, 8] manhatten for 3 = 2, 2 to the right, manhatten for 8 = 3, 1 left, 2 down, manhatten for 1 = 3, 2 up, 1 down
-    #[4, 5, 6]    [4, 5, 6] 
-    #[7, 8, 0]    [7, 1, 0]
-    #0, 2 should be at 2, 1
-    #Distance formula = |x2 - x1| + |y2 - y1|, so goal state row - misplaced row + goal state column - misplaced column,  | 2 - 0 | + | 1 - 2 | = 3
-    #https://cdn.codespeedy.com/wp-content/uploads/2020/03/manhattan.jpg
-    if (verbose):
-        print('Manhatten Distance Heuristic was selected.')
-    goal_row = 0
-    goal_column = 0
-    misplaced_row = 0
-    misplaced_column = 0
-    misplaced_tile = 0
-    manhatten = 0
-    for row in range(len(puzzle)):
-        for column in range(len(puzzle)):
-            if (puzzle[row][column] != goal_state[row][column]):
-                if (puzzle[row][column] != 0): #need to account for an empty tile
-                    misplaced_tile = puzzle[row][column] #Find the misplaced tile
-                    misplaced_row = row #Have the row
-                    misplaced_column = column #Have the column
-                    goal_row, goal_column = get_goal_position(goal_state, misplaced_tile) #Run our helper function to find our goal coordinates for the formula to work
-                    if (verbose):
-                        print('The misplaced tile ' + str(misplaced_tile) + ' is ' + str(abs(goal_row - misplaced_row) + abs(goal_column - misplaced_column)) + ' spaces away.') 
-                    manhatten += (abs(goal_row - misplaced_row) + abs(goal_column - misplaced_column)) #Uses the formula linked above
-    if (verbose):
-        print('The Manhatten Distance of the puzzle provided would be: ' + str(manhatten) + '\n')
-    return manhatten
 
 #HELPER FUNCTIONS
 #NEED DEEP COPY BECAUSE SHALLOW COPY SCREWED UP COPYING OBJECTS 
@@ -260,5 +210,56 @@ def print_puzzle(puzzle): #Helper function to print out the puzzle in a neat man
         print(str(puzzle[row][2]), end = "")
         print(']')
     print('\n')
+
+#HEURISTIC FUNCTIONS
+def uniform_cost():
+    if (verbose):
+        print('Uniform Cost Search was selected.')
+    return 0 #Heuristic is always 0
+
+def a_star_misplaced(puzzle):
+    if (verbose):
+        print('Misplaced Tile Heuristic was selected.')
+    misplaced_tiles = 0
+    for row in range(len(puzzle)): #Loop through the puzzle
+        for column in range(len(puzzle)):
+            if (puzzle[row][column] != goal_state[row][column]):
+                if (puzzle[row][column] != 0): #need to account for an empty tile
+                    if (verbose):
+                        print('The misplaced tile is ' + str(puzzle[row][column]))
+                    misplaced_tiles += 1
+    if (verbose):
+        print("A total of " + str(misplaced_tiles) + " misplaced tiles were found." + '\n')
+    return misplaced_tiles
+
+def a_star_manhatten(puzzle):
+    #[1, 2, 3]    [3, 2, 8] manhatten for 3 = 2, 2 to the right, manhatten for 8 = 3, 1 left, 2 down, manhatten for 1 = 3, 2 up, 1 down
+    #[4, 5, 6]    [4, 5, 6] 
+    #[7, 8, 0]    [7, 1, 0]
+    #0, 2 should be at 2, 1
+    #Distance formula = |x2 - x1| + |y2 - y1|, so goal state row - misplaced row + goal state column - misplaced column,  | 2 - 0 | + | 1 - 2 | = 3
+    #https://cdn.codespeedy.com/wp-content/uploads/2020/03/manhattan.jpg
+    if (verbose):
+        print('Manhatten Distance Heuristic was selected.')
+    goal_row = 0
+    goal_column = 0
+    misplaced_row = 0
+    misplaced_column = 0
+    misplaced_tile = 0
+    manhatten = 0
+    for row in range(len(puzzle)):
+        for column in range(len(puzzle)):
+            if (puzzle[row][column] != goal_state[row][column]):
+                if (puzzle[row][column] != 0): #need to account for an empty tile
+                    misplaced_tile = puzzle[row][column] #Find the misplaced tile
+                    misplaced_row = row #Have the row
+                    misplaced_column = column #Have the column
+                    goal_row, goal_column = get_goal_position(goal_state, misplaced_tile) #Run our helper function to find our goal coordinates for the formula to work
+                    if (verbose):
+                        print('The misplaced tile ' + str(misplaced_tile) + ' is ' + str(abs(goal_row - misplaced_row) + abs(goal_column - misplaced_column)) + ' spaces away.') 
+                    manhatten += (abs(goal_row - misplaced_row) + abs(goal_column - misplaced_column)) #Uses the formula linked above
+    if (verbose):
+        print('The Manhatten Distance of the puzzle provided would be: ' + str(manhatten) + '\n')
+    return manhatten
 
 main()
